f67ff85406c42a33602dc8a42a32abe3e98a01e0
sascov5-eng <sascov5@gmail.com>
2026-09-01 05:25:06 +0300

    feat: add Continue and New Game launcher

--- Sources/AppRoot.swift
+++ Sources/AppRoot.swift
@@ -5,7 +5,37 @@ import SpriteKit
 struct AshenHollowApp: App {
     var body: some Scene {
         WindowGroup {
-            GameView()
+            DemoLauncherView()
+        }
+    }
+}
+
+private struct DemoLauncherView: View {
+    @State private var launchMode: DemoLaunchMode?
+
+    var body: some View {
+        if let launchMode {
+            GameView(launchMode: launchMode)
+                .ignoresSafeArea()
+        } else {
+            VStack(spacing: 18) {
+                Text("ASHEN HOLLOW")
+                    .font(.title.bold())
+
+                if DemoSaveStore().hasSave {
+                    Button("CONTINUE") {
+                        launchMode = .continueGame
+                    }
+                }
+
+                Button("NEW GAME") {
+                    launchMode = .newGame
+                }
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.black)
+            .foregroundStyle(Color.white)
+            .ignoresSafeArea()
         }
     }
 }
