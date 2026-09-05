be47404f1e56261d4eca2a9c1d1e3711776dfd89
sascov5-eng <sascov5@gmail.com>
2026-09-05 03:34:18 +0300

    feat: add unmistakable V24 build marker

--- Sources/GameView.swift
+++ Sources/GameView.swift
@@ -17,12 +17,23 @@ struct GameView: View {
     }
 
     var body: some View {
-        SpriteView(
-            scene: scene,
-            options: [.ignoresSiblingOrder]
-        )
-        .ignoresSafeArea()
-        .background(Color.black)
+        ZStack(alignment: .top) {
+            SpriteView(
+                scene: scene,
+                options: [.ignoresSiblingOrder]
+            )
+            .ignoresSafeArea()
+            .background(Color.black)
+
+            Text("V24 BUILD TEST")
+                .font(.system(size: 18, weight: .heavy, design: .monospaced))
+                .foregroundColor(.yellow)
+                .padding(.horizontal, 12)
+                .padding(.vertical, 6)
+                .background(Color.black.opacity(0.85))
+                .padding(.top, 8)
+                .allowsHitTesting(false)
+        }
         .onAppear {
             installRuntimeWhenReady()
         }
